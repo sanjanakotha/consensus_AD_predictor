736fa0d3a44d79c031e2e74df8b83059734cac81
Sanjana Kotha <sanjanakotha@n0118.savio2>
2025-09-25 15:57:18 -0700

    Added plant tf seqs and ran metapredict

--- scripts/jobs/calc_seq_props.sh
+++ scripts/jobs/calc_seq_props.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=calc_seq_props_CL
+#SBATCH --job-name=calc_seq_props_CL_SK_plant
 #SBATCH --account=ac_stallerlab
 #SBATCH --partition=savio3
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
-#SBATCH --time=30:00
+#SBATCH --time=1:00:00
 #SBATCH --output=../logs/calc_seq_props_%j.log
 
 module load anaconda3/2024.02-1-11.4
@@ -17,5 +17,5 @@ source activate /global/scratch/projects/fc_mvslab/OpenProjects/Sanjana/conda/pa
 #     --output /global/scratch/projects/fc_mvslab/OpenProjects/Sanjana/consensus_AD_predictor/output/interpro_uniprot_evid_1_2_seq_props.csv
 
 python /global/scratch/projects/fc_mvslab/OpenProjects/Sanjana/consensus_AD_predictor/scripts/calculate_properties.py \
-    --input /global/scratch/projects/fc_mvslab/OpenProjects/Sanjana/consensus_AD_predictor/output/cl_seqs.fasta \
-    --output /global/scratch/projects/fc_mvslab/OpenProjects/Sanjana/consensus_AD_predictor/output/cl_seqs_seq_props.csv
+    --input /global/scratch/projects/fc_mvslab/OpenProjects/Sanjana/consensus_AD_predictor/data/CL_SK_plant_input.fasta \
+    --output /global/scratch/projects/fc_mvslab/OpenProjects/Sanjana/consensus_AD_predictor/output/CL_SK_plant_seq_props.csv
